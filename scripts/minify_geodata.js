c260885cdcd801ffc90ef03308717804e320dc64
Vinay <vbvyas@gmail.com>
2014-09-23 11:14:31 -0700

    Create sample zip.json

--- scripts/minify_geodata.js
+++ scripts/minify_geodata.js
@@ -1,3 +1,5 @@
+// Converts a geojson file into json file
+// list of { zip, coordinates }
 var fs = require('fs');
 var JSONStream = require('JSONStream');
 var es = require('event-stream');
@@ -8,8 +10,12 @@ writer.write('[');
 var reader = fs.createReadStream('zipgeodata.json');
 reader.pipe(JSONStream.parse('features.*'))
 .pipe(es.mapSync(function (data) {
+  // get zip code and it's corresponding coordinates
   var td = { zip: data.properties.GEOID10, coordinates: data.geometry.coordinates };
-  var stream = JSON.stringify(td, null, 2);
+  var stream = JSON.stringify(td, null, 2); // pretty print json
   stream += ',';
   writer.write(stream);
-}));
+}))
+.pipe(function () {
+  writer.write(']');
+});
