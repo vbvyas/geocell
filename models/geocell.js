f80565f80982b93efcc4ffed7bb5de6a5bd29a75
Vinay <vbvyas@gmail.com>
2014-09-29 21:09:56 -0700

    Create Geocell model

--- models/geocell.js
+++ models/geocell.js
@@ -1,1 +1,8 @@
 // geocell model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GeocellSchema = new Schema({
+  zip: { type: String, trim: true },
+  geocells: { type: [] }
+});
