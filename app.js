5ebfb56b279a6c0aae2a4a72f6efec5b7d874c2a
Vinay <vbvyas@gmail.com>
2014-10-01 21:56:04 -0700

    Retrieve geocell from db.

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var Hapi = require('hapi');
 require('sugar');
-var cells = require('./scripts/cells.json');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/geocell_dev');
+var Geocell = mongoose.model('Geocell');
+var cells = require('./scripts/cells.json');
 
 var app = Hapi.createServer('0.0.0.0', parseInt(process.env.PORT, 10) || 3000);
 
 function getZipCoordinates(req, res) {
   var zip = req.params.zip;
-  cells.find(function (cell) {
-    if (cell.zip == zip) {
-      res(cell);
+  Geocell.findOne({ zip: zip }, function (err, cell) {
+    if (err) {
+      cells.find(function (cell) {
+        if (cell.zip == zip) {
+          res(cell);
+        } else {
+          res({});
+        }
+      });
     } else {
-      res({});
+      res(cell);
     }
   });
 }
