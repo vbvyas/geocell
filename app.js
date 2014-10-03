17d97764262f374c53e9a85d797fe59ea1db2f43
Vinay <vbvyas@gmail.com>
2014-10-02 22:10:43 -0700

    Add comments

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ var app = Hapi.createServer('0.0.0.0', parseInt(process.env.PORT, 10) || 3000);
 
 function getZipCoordinates(req, res) {
   var zip = req.params.zip;
+  // check in db first
   Geocell.findOne({ zip: zip }, function (err, cell) {
     if (err) {
+      // check in static data file
       cells.find(function (cell) {
         if (cell.zip == zip) {
           res(cell);
